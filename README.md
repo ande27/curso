ee10b4f9965b3237e56aef2c11467025893cead4
ande27 <32495973+ande27@users.noreply.github.com>
2022-11-13 19:19:37 -0300

    Modificado o README e criando o arquivo Index

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # curso
 curso da alura.
+
+Sistema de cadastro de Jogos
+
+Status do projeto: Em desenvolvimento
+
+Para rodar esse projeto na sua máquina, por favor digite:
+
+node app.js
--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-Sistema de cadastro de Jogos
-
-Status do projeto: Em desenvolvimento
-
-Para rodar esse projeto na sua máquina, por favor digite:
-
-node app.js
+console.log("Rodando o sistema de gerenciamento de jogos")
